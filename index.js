d24bab038300243ef6aae89fccd1b3612798ee7e
immayurpanchal <mayurpanchal002@gmail.com>
2022-07-05 10:03:00 +0530

    1.1 bubble sort : swapping

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-import { LinkedList } from './src/LinkedList';
+// import { LinkedList } from './src/LinkedList';
 
-const ll = new LinkedList();
-ll.add(10);
-ll.add(20);
-ll.add(30);
-ll.add(40);
-ll.printList();
-ll.insertRec(0, 55);
-ll.printList();
+// const ll = new LinkedList();
+// ll.add(10);
+// ll.add(20);
+// ll.add(30);
+// ll.add(40);
+// ll.printList();
+// ll.insertRec(0, 55);
+// ll.printList();
+
+// Bubble Sort
+// Swap values of 2 variables
+let value1 = 10;
+let value2 = 20;
+
+// Before Swapping
+console.log(value1, value2);
+
+// Write Swapping Logic here - Start
+// 1.1 Initial problem when writing like below
+// value1 = value2;
+// value2 = value1;
+
+// 1.2 Now improve this using 3rd variable
+// let temp = value1;
+// value1 = value2;
+// value2 = temp;
+
+// 1.3 swapping without third variable
+// value1 = value1 + value2;
+// value2 = value1 - value2;
+// value1 = value1 - value2;
+
+// Swapping Logic - End
+// After Swapping
+console.log(value1, value2); // 20 10
---
+++ src/sorting/1_bubble.js
@@ -0,0 +1,12 @@
+export function bubbleSort(arr) {
+  // sort the array in place
+}
+
+// Learning Strategy
+// 1. Learn Value Swapping
+// Write a function that takes two variables and swap the values
+function swap(value1, value2) {
+  console.log(value1, value2);
+}
+
+swap(10, 20);
