6bfd7f75f9b24c33bbc34062e6bf89a81c6b9e9b
immayurpanchal <mayurpanchal002@gmail.com>
2022-07-10 10:08:21 +0530

    sum using recursion

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function factorial(n) {
-  if (n === 1) return 1;
-  return n * factorial(n - 1);
+function sum(n) {
+  if (n === 0) return 0;
+  return n + sum(n - 1);
 }
 
-console.log(factorial(5));
+console.log(sum(5));
